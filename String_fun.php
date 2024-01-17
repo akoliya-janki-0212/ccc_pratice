3f7598ba055756dd094425975b513f40f28b9432
janki akoliya <akoliyajanki0212@gmail.com>
2024-01-17 18:36:11 +0530

    string function

--- String_fun.php
+++ String_fun.php
@@ -4,10 +4,12 @@ $str="hello good morning";
 echo $str."<br />";
 
 //string length - strlen()
-echo "length of given string is : ".strlen($str);
-echo strlen('')."<br>";
-echo strlen(' ');
-echo strlen('trlen is a PHP function that returns the length of a string in characters. It can also be used to return true if the string is empty or false if the string is not empty. The strlen function is useful for determining the length of a string before concatenating it with another string or performing length-based operations. It is a built-in function in PHP and can be used to manipulate the characters in a string.').'<br>';
+// echo "length of given string is : ".strlen($str);
+// echo strlen('')."<br>";
+// echo strlen(' ')."<br>";
+//echo strlen('ghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhtrlen is a PHP function that returns the length of a string in characters. It can also be used to return true if the string is empty or false if the string is not empty. The strlen function is useful for determining the length of a string before concatenating it with another string or performing length-based operations. It is a built-in function in PHP and can be used to manipulate the characters in a string.').'<br>';
+
+
 //replace string - str_replace(search,newstr,str)
 //echo " new string is : ".str_replace("morning","evening",$str);
 
@@ -61,4 +63,22 @@ echo strlen('trlen is a PHP function that returns the length of a string in char
 
 //randomly shuffle all character in string - str_shuffle()
 //echo str_shuffle($str);
+
+// convert string into array - strsplit()
+// $arr=str_split($str);
+// foreach ($arr as $item) {
+//     echo $item,"<br>";
+//}
+
+//no of words  in string - str_word_count()
+//echo str_word_count($str);
+
+//replace text with in partion - substr_replace()
+// echo substr_replace($str,"hi",6);
+// echo substr_replace($str,"hi",6,4);
+
+// padding in string str_pad() 
+echo str_pad($str,22,"-");
+
+//echo strrev($str);
 ?>
