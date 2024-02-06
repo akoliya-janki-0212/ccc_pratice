26bc114e5ca39cb67c0aa9cc23b9a4ad5e1250a1
janki akoliya <akoliyajanki0212@gmail.com>
2024-02-06 18:17:36 +0530

    session_work

---
+++ tmp.php
@@ -0,0 +1,23 @@
+<?php
+$arr=array(1,3,5,7,9);
+// $tarr=$arr;
+// sort($tarr);
+// array_pop($tarr);
+// echo "min:".array_sum($tarr).'<br>';
+// print_r($tarr);
+// $tarr=$arr;
+// rsort($tarr);
+// print_r($tarr);
+// array_pop($tarr);
+// echo "max:".array_sum($tarr);
+
+// sort($arr);
+// $tarr=$arr;
+// array_splice($tarr,4,1);
+// echo "min:".array_sum($tarr).'<br>';
+// $tarr=$arr;
+// print_r($tarr);
+// array_splice($tarr,0,1);
+// print_r($tarr);
+// echo "max:".array_sum($tarr).'<br>';
+?>
